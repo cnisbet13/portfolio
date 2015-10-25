3869ccb0a9f54bc97cc6f01c1c79d567978ca729
Calvin Nisbet <calvinnisbet@Calvins-MacBook-Air.local>
2015-10-25 00:40:40 -0700

    first commit

---
+++ jquery.diagram.js
@@ -0,0 +1,9 @@
+$('.demo').diagram({
+size: "200", // graph size
+borderWidth: "20", // border width
+bgFill: "#95a5a6", // background color
+frFill: "#1abc9c", // foreground color
+textSize: 54, // text color
+font: "serif", // font
+textColor: '#2a2a2a' // text color
+});
